ea87ead62ef5390158b7c97e62aa5384194c12ef
H K Charan <hkcharan08@gmail.com>
2024-12-13 19:53:30 +0530

    Changes

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,14 @@
+import User from "../models/userSchema.js";
+
+const register = async (req, res) => {
+    try {
+        const {name,email,password} = req.body;
+        const user = await User.create({
+            name,email,password
+        })
+
+        res.status(200).json({message:"User Created"})
+    } catch (error) {
+        res.status(500).json({message:"Error occurred during registration!"})
+    }
+}
