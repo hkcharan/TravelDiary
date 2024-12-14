f5dd17ded8806c9dcf3f660b34a701702ac0a2fc
H K Charan <hkcharan08@gmail.com>
2024-12-14 15:59:40 +0530

    changes

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,14 +1,43 @@
-import {User} from "../models/userSchema.js";
+import { User } from "../models/userSchema.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
-    try {
-        const {name,email,password} = req.body;
-        const user = await User.create({
-            name,email,password
-        })
+  try {
+    const { name, email, password } = req.body;
 
-        res.status(200).json({message:"User Created"})
-    } catch (error) {
-        res.status(500).json({message:"Error occurred during registration!"})
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required!" });
     }
-}
+
+    const isUser = await User.findOne({ email });
+
+    if (isUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign({userId:user._id},process.env.ACCESS_TOKEN,{
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRE
+    })
+
+    return res.status(201).json({
+        user,
+        accessToken,
+        message:"User Registred"
+    })
+
+
+  } catch (error) {
+    res.status(500).json({ message: "Error occurred during registration!" });
+  }
+};
