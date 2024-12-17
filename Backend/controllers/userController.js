bd0ea5855a8d72467e4217446c116a30e6f7fb51
H K Charan <hkcharan08@gmail.com>
2024-12-17 20:07:49 +0530

    Changes

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -38,6 +38,6 @@ export const register = async (req, res) => {
 
 
   } catch (error) {
-    res.status(500).json({ message: "Error occurred during registration!" });
+    res.status(500).json({ message: "Error occurred during registration" });
   }
 };
